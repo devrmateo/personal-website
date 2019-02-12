add0ecc407eb0c64f8d083fb5c003d1b39d5f7b0
Matthew Smith <mateoelcule@gmail.com>
2019-02-11 22:36:28 -0800

    refactor: Add link to Medium article on homepage

--- index.php
+++ index.php
@@ -17,13 +17,14 @@
             <img class="profile" src="images/profile.jpg">
           </div>
           <p class="first">I am a detail-oriented Front-End Web Developer with certifications in HTML, CSS, JavaScript, jQuery, PHP, and MySQL. Recently, I completed Udacity's Front-End Web Developer Nanodegree, for which I earned a full scholarship through the Grow With Google Initiative.</p>
-          <p>Before becoming a web developer, I taught college Spanish for twenty years.  This prior experience in the humanities lends me a unique insight into the tech world (see the article in the "Related" section).  It is clear that you need to have the requisite knowledge and skills to be a successful developer.  Nonetheless, the manner in which you approach problems is even more important.  I utilize my well-honed critical thinking skills to identify potential challenges in any project that I am working on, and then I ask the right questions to go about overcoming them.</p>
-          <p>Explore this website to get to know a little bit more about me.  If you're interested, get in touch using the contact form.  I look forward to helping you add value to your personal life, business or organization.</p>
+          <p>Before becoming a web developer, I taught college Spanish for almost twenty years.  I wrote a blog post about my career transition, which recently got published on Udacity's Medium publication (click on the link in the "Related" section to read it).
+          <p>My prior experience in the humanities lends me a unique insight into the tech world (see the linked article in the "Related" section).  It is clear that you need to have the requisite knowledge and skills to be a successful developer.  Nonetheless, the manner in which you approach problems is even more important.  I utilize my well-honed critical thinking skills to identify potential problems in any project that I am working on, and then I ask the right questions to go about solving them.</p>
         </section>
         <section class="subNavPanel">
           <h4 class="subNavHeader">Related</h4>
           <nav class="subNav">
             <ul class="related">
+               <li class="relatedLi"><a class="subNavLink" href="https://medium.com/udacity/my-journey-from-spanish-instructor-to-web-developer-went-through-udacity-f18e522b2406" target="_blank">My Journey from Spanish Instructor to Web Developer With Udacity</a></li>
               <li class="relatedLi"><a class="subNavLink" href="https://hbr.org/2017/07/liberal-arts-in-the-data-age" target="_blank">Liberal Arts Majors Are the Future of the Tech Industry</a></li>
             </ul>
             <small>–Harvard Business Review</small>
