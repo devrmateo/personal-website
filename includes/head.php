7a8df778fe8b32c27aa34c6416ce0e3f07c12574
Matthew Smith <mateoelcule@gmail.com>
2019-12-27 21:23:02 -0800

    refactor: Update footer icons & include GitLab

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,5 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1">
-<script src="https://use.fontawesome.com/5485d530fa.js"></script>
+<script src="https://kit.fontawesome.com/76a0066f00.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="css/main.css">
 <link rel="stylesheet" media="screen and (min-width: 900px)" href="css/900.css">
