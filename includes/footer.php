47fa1b62d8ba74e34da6cc695e02ab382456c4af
Matthew Smith <mateoelcule@gmail.com>
2019-12-29 21:16:01 -0800

    refactor: Make social icons accessible

--- includes/footer.php
+++ includes/footer.php
@@ -5,10 +5,10 @@
   <a href="contact.php" class="footerNavLink">Contact</a>
 </nav>
 <div class="social">
-  <a href="https://github.com/devrmateo" target="_blank"><i class="fab fa-github fa-lg"></i></a>
-  <a href="https://gitlab.com/devrmateo" target="_blank"><i class="fab fa-gitlab fa-lg"></i></a>
-  <a href="https://www.linkedin.com/in/devrmateo/" target="_blank"><i class="fab fa-linkedin-in fa-lg"></i></a>  
-  <a href="https://twitter.com/devrmateo" target="_blank"><i class="fab fa-twitter fa-lg"></i></a>
+  <a href="https://github.com/devrmateo" target="_blank"><i class="fab fa-github fa-lg" aria-hidden="true" title="GitHub"></i><span class="sr-only">GitHub</span></a>
+  <a href="https://gitlab.com/devrmateo" target="_blank"><i class="fab fa-gitlab fa-lg" aria-hidden="true" title="GitLab"><span class="sr-only">GitLab</span></i></a>
+  <a href="https://www.linkedin.com/in/devrmateo/" target="_blank"><i class="fab fa-linkedin-in fa-lg" aria-hidden="true" title="LinkedIn"></i><span class="sr-only">LinkedIn</span></a>
+  <a href="https://twitter.com/devrmateo" target="_blank"><i class="fab fa-twitter fa-lg" aria-hidden="true" title="Twitter"></i><span class="sr-only">Twitter</span></a>
 </div>
 
   <div class="copyright">© 2019 Matthew Price Smith</div>
