f3d4292af0fb8377e0399172e83f73ec366cfdd4
Matthew Smith <mateoelcule@gmail.com>
2021-03-18 16:57:09 -0700

    style: Reformat js using Prettier

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ const main = document.querySelector('main');
 hamburgerMenu.addEventListener('click', toggle);
 main.addEventListener('click', close);
 
-function toggle () {
+function toggle() {
   document.querySelector('.drawer').classList.toggle('open');
 }
 
-function close () {
+function close() {
   document.querySelector('.drawer').classList.remove('open');
 }
